8f5f39f049aa1bafd388d92569b612e9849ffddb
Gabriel <1gabrielBrasil@gmail.com>
2021-09-29 20:23:26 -0300

    new files

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/gabriel/Desktop/Eletronica-Embarcado/src
+# Build directory: /home/gabriel/Desktop/Eletronica-Embarcado/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/gabriel/Desktop/Eletronica-Embarcado/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/gabriel/Desktop/Eletronica-Embarcado/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/gabriel/Desktop/Eletronica-Embarcado/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/gabriel/Desktop/Eletronica-Embarcado/src:$ROS_PACKAGE_PATH"
